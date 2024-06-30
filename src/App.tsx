8ef8fc6ecfc359e2dd4fb0753a1d9871326c84fe
yousakamori <yousakamori@gmail.com>
2024-06-30 10:43:14 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen w-full bg-neutral-900 text-neutral-50">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus dolorum
+      nihil architecto assumenda quibusdam suscipit itaque quos, repudiandae a
+      quod facere ipsum sint fugiat, est dolor cumque culpa consequatur minus!
+    </div>
+  );
+}
+
+export default App;
